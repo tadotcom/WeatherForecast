3344803e1de9aafd876fe6d3146c348bb5e41f8c
tadotcom <kh.sk.ti.1820@gmail.com>
2026-01-18 14:18:29 +0900

    feat: 現在地対応のNavigation定義を追加

--- app/src/main/java/com/miyazaki/weatherforecast/presentation/navigation/Screen.kt
+++ app/src/main/java/com/miyazaki/weatherforecast/presentation/navigation/Screen.kt
@@ -1,11 +1,19 @@
 package com.miyazaki.weatherforecast.presentation.navigation
 
+/**
+ * アプリ内の画面遷移を定義
+ */
 sealed class Screen(val route: String) {
     // ホーム画面
     data object Home : Screen("home_screen")
 
-    // 天気画面
+    /** 天気画面(都市名指定) */
     data object Weather : Screen("weather_screen/{city}") {
         fun createRoute(city: String) = "weather_screen/$city"
+    }
+
+    /** 天気画面(現在地・緯度経度指定) */
+    data object WeatherLocation : Screen("weather_location_screen/{lat}/{lon}") {
+        fun createRoute(lat: Double, lon: Double) = "weather_location_screen/$lat/$lon"
     }
 }
