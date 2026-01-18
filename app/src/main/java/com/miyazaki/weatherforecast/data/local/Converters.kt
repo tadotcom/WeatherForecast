32e491509d95e233f26effe3cb3b4c73f26f97e1
tadotcom <kh.sk.ti.1820@gmail.com>
2026-01-18 23:51:12 +0900

    refactor: Gsonを使っていたところをMoshiを使用するように修正

--- app/src/main/java/com/miyazaki/weatherforecast/data/local/Converters.kt
+++ app/src/main/java/com/miyazaki/weatherforecast/data/local/Converters.kt
@@ -1,21 +1,31 @@
 package com.miyazaki.weatherforecast.data.local
 
 import androidx.room.TypeConverter
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import com.miyazaki.weatherforecast.domain.model.WeatherInfo
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class Converters {
-    private val gson = Gson()
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val listType = Types.newParameterizedType(List::class.java, WeatherInfo::class.java)
+    private val adapter = moshi.adapter<List<WeatherInfo>>(listType)
 
     @TypeConverter
     fun fromWeatherInfoList(value: List<WeatherInfo>): String {
-        return gson.toJson(value)
+        return adapter.toJson(value)
     }
 
     @TypeConverter
     fun toWeatherInfoList(value: String): List<WeatherInfo> {
-        val listType = object : TypeToken<List<WeatherInfo>>() {}.type
-        return gson.fromJson(value, listType)
+        return try {
+            adapter.fromJson(value) ?: emptyList()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emptyList()
+        }
     }
 }
